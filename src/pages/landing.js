98001f30c94b509b2961aca8b9b7ed47c78305ae
Eduardo Ordaz <ieduuardo@outlook.com>
2023-11-22 00:25:55 -0600

    Se añaden camb vis., logos, href, favicon, meta

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -26,19 +26,19 @@ const Landing = () => {
     return (
     <main>
       <header>
-          <div className={'container'}>
+          <div className={'container'} id='header'>
               <nav className={`nav ${scrollTop > 80 ? 'scrolling' : ''}`}>
                  <div className={'nav-container'}>
                      <div className={'nav-social-media'}>
                          <ul>
-                             <li><NavLink to={'/'}><FaInstagram /></NavLink></li>
-                             <li><NavLink to={'/'}><FaFacebook /></NavLink></li>
-                             <li><NavLink to={'/'}><FaXTwitter /></NavLink></li>
-                             <li><NavLink to={'/'}><FaTiktok /></NavLink></li>
+                             <li><NavLink to={'https://www.instagram.com/editorial_inefable/'}><FaInstagram /></NavLink></li>
+                             <li><NavLink to={'https://www.facebook.com/editorialinefable'}><FaFacebook /></NavLink></li>
+                             <li><NavLink to={'https://twitter.com/InefableLibros'}><FaXTwitter /></NavLink></li>
+                             <li><NavLink to={'http://www.tiktok.com/@inefablelibros'}><FaTiktok /></NavLink></li>
                          </ul>
                      </div>
-                     <div className={'nav-logo'}><img alt={''} src={logo} /></div>
-                     <div className={'nav-contact'}><NavLink to={'/contacto'}>Contacto</NavLink></div>
+                     <div className={'nav-logo'}><a href='#header'><img alt={''} src={logo} /></a></div>
+                     <div className={'nav-contact'}><a href="#contacto">Contacto</a></div>
                  </div>
               </nav>
           </div>
@@ -57,12 +57,12 @@ const Landing = () => {
                 <div className={'bio-container'}>
                     <div className={'bio-info'}>
                         <h2>Ricardo Ravelo Galó</h2>
-                        <p>Es un periodista mexicano quien inicio su carrera como reportero en el año 1987, trabajando en los periódicos El Dictamen, La Nación, El Diario Sur, Diario del Istmo, revista Llave y Notiver.
-                            <br />En 1991 empezó a trabajar en la revista Proceso donde se le asignó la fuente policiaca. Actualmente se dedica a la investigación de temas relacionados con el narcotráfico, la justicia y la seguridad. El 9 de abril de 2011, recibió un homenaje en la
+                        <p>Es un periodista mexicano quien inicio su carrera como reportero en el año 1987, trabajando en los periódicos El Dictamen, La Nación, El Diario Sur, Diario del Istmo, revista Llave y Notiver.
+                            <br />En 1991 empezó a trabajar en la revista Proceso donde se le asignó la fuente policiaca. Actualmente se dedica a la investigación de temas relacionados con el narcotráfico, la justicia y la seguridad. <br/>El 9 de abril de 2011, recibió un homenaje en la
                             ciudad de Cosamaloapan donde, en sesión solemne
-                            de cabildo, las autoridades lo declararon «Hijo Predilecto».
+                            de cabildo, las autoridades lo declararon «Hijo Predilecto».
                             <br/><br/>
-                            En 2013 fue galardonado con el premio Rodolfo Walsh, que se concede a obras de no ficción del género negro, por su obra Narcomex: historia e historias de una guerra, en Gijón, España.</p>
+                            En 2013 fue galardonado con el premio Rodolfo Walsh, que se concede a obras de no ficción del género negro, por su obra Narcomex: historia e historias de una guerra, en Gijón, España.</p>
                     </div>
                     <div className={'bio-img'}><img alt={''} src={bio} /></div>
                 </div>
@@ -73,36 +73,36 @@ const Landing = () => {
                 <div className={'book-container'}>
                     <div className={'book-info'}>
                         <h2>El amo <br />de Jalisco</h2>
-                        <h3>UN GOBIERNO CON <br/>ESTRUCTURA </h3>
+                        <h3>UN GOBIERNO CON <br/>ESTRUCTURA CRIMINAL</h3>
                         <div className={'book-info-img'}><img alt={''} src={bookSmall} /></div>
                     </div>
                     <div className={'book-text'}>
                         <p>
-                            México podría ser calificado de un país muy exitoso en lo que se denomina “gatopardismo”: cuando todo cambia, pero sigue igual o peor. Es así cuando hablamos del poder que ejerce el crimen organizado en vastas zonas del país, con el amparo o complicidad del poder político en todos los niveles. El viejo régimen parece agonizar en numerosos estados del país, pero el poder del crimen sigue fortaleciéndose.
+                            México podría ser calificado de un país muy exitoso en lo que se denomina “gatopardismo”: cuando todo cambia, pero sigue igual o peor. Es así cuando hablamos del poder que ejerce el crimen organizado en vastas zonas del país, con el amparo o complicidad del poder político en todos los niveles. El viejo régimen parece agonizar en numerosos estados del país, pero el poder del crimen sigue fortaleciéndose.
                             <br /><br />
-                            En esta obra de Ricardo Ravelo, uno de los periodistas más reconocidos y valientes de México, se documenta cómo conviven la delincuencia organizada y el poder político en el Estado de Jalisco. Gobernado por el ingeniero Enrique Alfaro, el estado se ha convertido en una de las entidades más violentas de la nación, cuyas mafias y cárteles controlan hasta el 80 por ciento de su territorio y se han expandido por vastas zonas del país, desapareciendo personas de personas, sembrando muerte y corrupción. De persistir esta tendencia, el país puede convertirse en un estado fallido, ante la nulidad de las autoridades locales y federales.
+                            En esta obra de Ricardo Ravelo, uno de los periodistas más reconocidos y valientes de México, se documenta cómo conviven la delincuencia organizada y el poder político en el Estado de Jalisco. Gobernado por el ingeniero Enrique Alfaro, el estado se ha convertido en una de las entidades más violentas de la nación, cuyas mafias y cárteles controlan hasta el 80 por ciento de su territorio y se han expandido por vastas zonas del país, desapareciendo personas de personas, sembrando muerte y corrupción. De persistir esta tendencia, el país puede convertirse en un estado fallido, ante la nulidad de las autoridades locales y federales.
                             <br /><br />
-                            Intolerante a la crítica periodística, el gobernador Enrique Alfaro se ha confrontado con diversos periodistas que han cuestionado su desgobierno. Mediante amenazas directas, o bien advirtiendo con interponer denuncias penales, el mandatario se ha mostrado violento, insultante y bravucón ante la prensa crítica estatal y nacional. No soporta que le cuestionen sobre sus negocios. Mucho menos que le señalen sus errores y desatinos en materia de seguridad. El amo de Jalisco es una clara y dolorosa muestra del México de nuestro tiempo.
+                            Intolerante a la crítica periodística, el gobernador Enrique Alfaro se ha confrontado con diversos periodistas que han cuestionado su desgobierno. Mediante amenazas directas, o bien advirtiendo con interponer denuncias penales, el mandatario se ha mostrado violento, insultante y bravucón ante la prensa crítica estatal y nacional. No soporta que le cuestionen sobre sus negocios. Mucho menos que le señalen sus errores y desatinos en materia de seguridad. El amo de Jalisco es una clara y dolorosa muestra del México de nuestro tiempo.
                         </p>
                     </div>
                 </div>
             </div>
         </section>
-        <footer>
+        <footer id="contacto">
             <div className={'container'}>
                 <div className={'footer'}>
                     <div className={'left'}>
                         <img alt={''} src={footer} />
                         <div className={'left-bottom'}>
-                            <p>Mitikah, Av. Río Churubusco 601, Xoco,</p>
-                            <p>Coyoacán, 03330 Ciudad de México, CDMX</p>
+                            <a href='https://maps.app.goo.gl/GeVQMuJdzjP2TpSu8' target="_blank" rel="noreferrer">Mitikah, Av. Río Churubusco 601, Xoco,</a>
+                            <a href='https://maps.app.goo.gl/GeVQMuJdzjP2TpSu8' target="_blank" rel="noreferrer">Coyoacán, 03330 Ciudad de México, CDMX</a>
                         </div>
                     </div>
                     <div className={'center'}><img alt={''} src={inefable} /></div>
                     <div className={'right'}>
                         <div>
-                            <p>contacto@editorialinefable.com</p>
-                            <p>55 4770 7728</p>
+                            <a href='mailto:contacto@inefable.mx'>contacto@editorialinefable.com</a>
+                            <a href="tel:5547707728">55 4770 7728</a>
                         </div>
                     </div>
                 </div>
